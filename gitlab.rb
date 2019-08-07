5f5069dcb49eec420da02f6c86eae8849276f83d
aallali <44850191+aallali@users.noreply.github.com>
2019-08-07 17:29:13 +0100

    Update gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -4,5 +4,5 @@ nginx['redirect_http_to_https'] = true
 nginx['redirect_http_to_https_port'] = 80
 nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.crt"
 nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.key"
-gitlab_rails['gitlab_shell_ssh_port'] = 22
+gitlab_rails['gitlab_shell_ssh_port'] = 2222
 gitlab_rails['auto_migrate'] = false
